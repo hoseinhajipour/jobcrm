7afc8b4a766b15358275a1613d2ab908fd399266
hajipour <hoseinhajepor@gmail.com>
2023-06-13 23:20:12 +0330

    update em singup form

--- app/Components/Auth/SingupEm.php
+++ app/Components/Auth/SingupEm.php
@@ -2,19 +2,83 @@
 
 namespace App\Components\Auth;
 
+use App\Models\User;
+use App\Providers\RouteServiceProvider;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 use Livewire\Component;
+use Livewire\WithFileUploads;
 
 class SingupEm extends Component
 {
+    use WithFileUploads;
+
+    public $avatar;
+    public $name, $email, $password, $password_confirmation;
+    public $establishment_date, $representative_name;
+    public $business_number, $contact_person_number;
+    public $contact_person_name, $number_of_employees;
+    public $business_information, $sectors, $company_website_address;
+    public $company_type, $take, $capital, $Listed_or_not;
+    public $address;
+
+    public $agree = false;
+
+    public function rules()
+    {
+        return [
+            'name' => ['required'],
+            'email' => ['required', 'email', 'unique:users'],
+            'password' => ['required', 'confirmed'],
+        ];
+    }
+
     public function route()
     {
         return Route::get('/singup-em')
             ->name('SingupEm')
             ->middleware('guest');
     }
+
     public function render()
     {
         return view('auth.singup-em');
+    }
+
+    public function register()
+    {
+        $this->validate();
+
+        if (!$this->honeyPasses()) {
+            return null;
+        }
+
+        $user = User::create([
+            'name' => $this->name,
+            'email' => $this->email,
+            'establishment_date' => $this->establishment_date,
+            'representative_name' => $this->representative_name,
+            'business_number' => $this->business_number,
+            'contact_person_number' => $this->contact_person_number,
+            'contact_person_name' => $this->contact_person_name,
+            'number_of_employees' => $this->number_of_employees,
+            'business_information' => $this->business_information,
+            'sectors' => $this->sectors,
+            'company_website_address' => $this->company_website_address,
+            'company_type' => $this->company_type,
+            'take' => $this->take,
+            'capital' => $this->capital,
+            'Listed_or_not' => $this->Listed_or_not,
+            'address' => $this->address,
+            'password' => bcrypt($this->password),
+        ]);
+        if ($this->avatar) {
+            $path = $this->avatar->store('avatars');
+            $user->avatar = $path;
+            $user->save();
+        }
+        Auth::login($user, true);
+
+        return redirect()->to(RouteServiceProvider::profile);
     }
 }
--- resources/views/auth/singup-em.blade.php
+++ resources/views/auth/singup-em.blade.php
@@ -13,15 +13,13 @@
             </div>
 
 
-
-
             <!-- Form START -->
-            <form class="file-upload" action="javascript:alert('가입이 성공적으로 저장되었습니다!');" id="form">
+            <form class="file-upload" wire:submit.prevent="register">
 
 
                 <div class="col-xxl-12 px-4 py-3 rounded ">
 
-                    <ul class="nav nav-pills nav-fill"  id="myTab" >
+                    <ul class="nav nav-pills nav-fill" id="myTab">
                         <li class="nav-item">
                             <a class="nav-link " href="{{route('SingupJs')}}">개인회원</a>
                         </li>
@@ -48,12 +46,11 @@
                                         <i class="icofont-user position-absolute top-50 start-50 translate-middle text-secondary"></i>
                                     </div>
                                     <!-- Button -->
-                                    <input type="file" id="customFile" name="file" hidden="">
+                                    <input type="file" wire:model="avatar" id="customFile" name="file" hidden="">
                                     <label class="btn btn-success-soft btn-block" for="customFile">기업 로고 업로드</label>
                                     <!-- Content -->
                                     <p class="text-muted mt-3 mb-0"><span class="me-1"></span>최소 크기 300px x 300px</p>
                                 </div>
-
 
 
                             </div>
@@ -73,19 +70,24 @@
                                 <!--  Name -->
                                 <div class="col-md-6">
                                     <label class="form-label">회사명 *</label>
-                                    <input type="text" class="form-control" id="companyname" placeholder="" aria-label="First name" value="회사명 ...">
+                                    <input type="text" wire:model.defer="name"
+                                           class="form-control" id="companyname"
+                                           placeholder="" aria-label="First name" value="회사명 ...">
+                                    @error('name') <span class="invalid-input">{{ $message }}</span> @enderror
                                 </div>
 
 
                                 <!-- New password -->
                                 <div class="col-md-6">
                                     <label for="exampleInputPassword1" class="form-label"> 비밀번호</label>
-                                    <input type="password" class="form-control" id="exampleInputPassword1">
+                                    <input type="password" wire:model.defer="password" class="form-control" id="exampleInputPassword1">
+                                    @error('password') <span class="invalid-input">{{ $message }}</span> @enderror
                                 </div>
                                 <!-- Confirm password -->
                                 <div class="col-md-6">
                                     <label for="exampleInputPassword2" class="form-label"> 비밀번호 재입력</label>
-                                    <input type="password" class="form-control" id="exampleInputPassword2">
+                                    <input type="password" wire:model.defer="password_confirmation" class="form-control" id="exampleInputPassword2">
+                                    @error('password_confirmation') <span class="invalid-input">{{ $message }}</span> @enderror
                                 </div>
 
 
@@ -95,7 +97,7 @@
 
                                     <div class="input-group date" id="datepicker">
 
-                                        <input type="text" class="form-control">
+                                        <input type="date" wire:model.defer="establishment_date" class="form-control">
 
                                         <span class="input-group-append">
                                       <span class="input-group-text bg-white">
@@ -108,37 +110,49 @@
                                 <!--  representative name -->
                                 <div class="col-md-6">
                                     <label class="form-label">대표자명 *</label>
-                                    <input type="text" class="form-control" id="representativename" placeholder="" aria-label="First name" value="대표자명 ...">
+                                    <input type="text" wire:model.defer="representative_name"
+                                           class="form-control" id="representativename"
+                                           placeholder="" aria-label="First name" value="대표자명 ...">
                                 </div>
 
 
                                 <!-- Business Number -->
                                 <div class="col-md-6">
                                     <label class="form-label">사업자번호 *</label>
-                                    <input type="text" class="form-control" id="businessnumber" placeholder="" aria-label="Phone number" value="000 000 00000">
+                                    <input type="text" class="form-control"
+                                           wire:model.defer="business_number"
+                                           id="businessnumber" placeholder="" aria-label="Phone number" value="000 000 00000">
                                 </div>
                                 <!-- Email -->
                                 <div class="col-md-6">
                                     <label for="inputEmail4" class="form-label">E-mail 주소 *</label>
-                                    <input type="email" class="form-control" id="email" value="예: ATR@allthatreception.com">
+                                    <input type="email"
+                                           wire:model.defer="email"
+                                           class="form-control" id="email" value="예: ATR@allthatreception.com">
                                 </div>
 
                                 <!-- Address -->
                                 <div class="col-md-6">
                                     <label class="form-label">주소 *</label>
-                                    <input type="text" class="form-control" id="address" placeholder="" aria-label="Address" value="예: 06682,서울시 서초구 방배로19길 17 (방배동) 서울빌딩 3층">
+                                    <input type="text" class="form-control"
+                                           wire:model.defer="address"
+                                           id="address" placeholder="" aria-label="Address" value="예: 06682,서울시 서초구 방배로19길 17 (방배동) 서울빌딩 3층">
                                 </div>
 
                                 <!-- contact number -->
                                 <div class="col-md-6">
                                     <label class="form-label">담당자번호 *</label>
-                                    <input type="text" class="form-control" id="contactnumber" placeholder="" aria-label="Phone number" value="+82 10 1234 5678">
+                                    <input type="text" class="form-control"
+                                           wire:model.defer="contact_person_number"
+                                           id="contactnumber" placeholder="" aria-label="Phone number" value="+82 10 1234 5678">
                                 </div>
 
                                 <!--  Name of person in charge -->
                                 <div class="col-md-6">
                                     <label class="form-label">담당자명 *</label>
-                                    <input type="text" class="form-control" id="nameofpersonincharge" placeholder="" aria-label="First name" value="담당자명 ...">
+                                    <input type="text" class="form-control"
+                                           wire:model.defer="contact_person_name"
+                                           id="nameofpersonincharge" placeholder="" aria-label="First name" value="담당자명 ...">
                                 </div>
 
                             </div> <!-- Row END -->
@@ -163,39 +177,53 @@
                                 <!--  number of employees -->
                                 <div class="col-md-6">
                                     <label class="form-label">직원수</label>
-                                    <input type="text" class="form-control" id="numberofemployees" placeholder="" aria-label="title" value="____ 명">
+                                    <input type="text" class="form-control"
+                                           wire:model.defer="number_of_employees"
+                                           id="numberofemployees" placeholder="" aria-label="title" value="____ 명">
                                 </div>
                                 <!--  business information -->
                                 <div class="col-md-6">
                                     <label class="form-label">사업내용 *</label>
-                                    <input type="text" class="form-control" id="businessinformation" placeholder="" aria-label="title" value="예: 서비스">
+                                    <input type="text" class="form-control"
+                                           wire:model.defer="business_information"
+                                           id="businessinformation" placeholder="" aria-label="title" value="예: 서비스">
                                 </div>
                                 <!--  Sectors -->
                                 <div class="col-md-6">
                                     <label class="form-label">업종 *</label>
-                                    <input type="text" class="form-control" id="sectors" placeholder="" aria-label="title" value="예: 서비스">
+                                    <input type="text" class="form-control"
+                                           wire:model.defer="sectors"
+                                           id="sectors" placeholder="" aria-label="title" value="예: 서비스">
                                 </div>
                                 <!--  home page -->
                                 <div class="col-md-6">
                                     <label class="form-label">기업 홈페이지 주소 *</label>
-                                    <input type="text" class="form-control" id="homepage" placeholder="" aria-label="title" value="예: http://www.allthatmodel.co.kr">
+                                    <input type="text" class="form-control"
+                                           wire:model.defer="company_website_address"
+                                           id="homepage" placeholder="" aria-label="title" value="예: http://www.allthatmodel.co.kr">
                                 </div>
                                 <!--  business type -->
                                 <div class="col-md-6">
                                     <label class="form-label">기업형태 *</label>
-                                    <input type="text" class="form-control" id="businesstype" placeholder="" aria-label="title" value="...">
+                                    <input type="text" class="form-control"
+                                           wire:model.defer="company_type"
+                                           id="businesstype" placeholder="" aria-label="title" value="...">
                                 </div>
                                 <!--  sales -->
                                 <div class="col-md-6">
                                     <label class="form-label">매출액</label>
-                                    <input type="text" class="form-control" id="sales" placeholder="" aria-label="title" value="____ 억">
+                                    <input type="text" class="form-control"
+                                           wire:model.defer="take"
+                                           id="sales" placeholder="" aria-label="title" value="____ 억">
                                 </div>
 
 
                                 <!--  capital -->
                                 <div class="col-md-6">
                                     <label class="form-label">자본금</label>
-                                    <input type="text" class="form-control" id="capital" placeholder="" aria-label="title" value="____ 억">
+                                    <input type="text"
+                                           wire:model.defer="capital"
+                                           class="form-control" id="capital" placeholder="" aria-label="title" value="____ 억">
                                 </div>
 
 
@@ -203,7 +231,7 @@
                                 <div class="col-md-6">
                                     <label class="form-label">상장여부</label>
 
-                                    <select class="custom-select" id="crape">
+                                    <select wire:model.defer="Listed_or_not" class="custom-select" id="crape">
                                         <option selected>선택...</option>
                                         <option class="form-control" value="1">상장</option>
                                         <option class="form-control" value="2">비상장</option>
@@ -223,8 +251,9 @@
                                     <div class="bg-secondary-soft px-3 py-4 rounded">
                                         <div class="row g-3">
                                             <div class="form-check">
-                                                <input type="checkbox" class="form-check-input" id="exampleCheck1">
-                                                <label class="form-check-label" for="exampleCheck1">약관동의</label>
+                                                <input class="agree-check"
+                                                       type="checkbox" wire:model="agree">
+                                                <label for="exampleCheck1">약관동의</label>
                                             </div>
                                         </div>
                                     </div>
@@ -238,17 +267,16 @@
                 </div>
 
 
-
-
                 <!-- button -->
                 <div class="gap-3 d-md-flex justify-content-md-end text-center">
-                    <!--	<button type="button" class="btn btn-danger btn-lg">Delete profile</button> -->
-                    <button type="submit" class="btn btn-primary btn-lg" id="emsignup">등록</button>
+                    <button type="submit" {{ $agree ? '' : 'disabled' }}
+                    class="btn btn-primary btn-lg" id="emsignup">등록</button>
                 </div>
 
-                <br /> <br /> <br />
+                <br/> <br/> <br/>
 
-            </form> <!-- Form END -->
+            </form>
+            <!-- Form END -->
         </div>
     </div>
 </div>
